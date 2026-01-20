8400260c6006fc6e903eec2ed7842dbf24f4cdbd
unkn0wn-j <wjsrnr470@gmail.com>
2026-01-20 15:06:29 +0900

    Fast API - Project Setup

---
+++ app/db/base.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# 파일명을 명시적으로 지정
+load_dotenv(".env.dev")
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
