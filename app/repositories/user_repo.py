e8591cdfaff470c614715a21ca5fc598efb3ca1e
wooryun <rhrjs4@naver.com>
2026-01-23 13:10:15 +0900

    🚑 hotfix : 회원가입 시 user_id와 user_name이 일치하지 않을 때 기능 실행이 되지 않는 현상 수정

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -2,7 +2,7 @@ from app.models.user import User
 
 
 async def get_user_by_username(username: str):
-    return await User.get_or_none(user_name=username)
+    return await User.get_or_none(user_id=username)
 
 
 async def logout_user(user_id: str):
